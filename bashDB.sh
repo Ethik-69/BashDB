c8ad4b296b17fba00ccd774f7ec44111f50b13aa
Ethan Chamik <ethancmk@hotmail.fr>
2018-07-04 16:13:45 +0200

    add db file + switch/case

--- bashDB.sh
+++ bashDB.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+case $1 in
+    -w)
+        echo 'Write' ;;
+    -r)
+        echo 'read' ;;
+    -d)
+        echo 'delete' ;;
+    *)
+        echo 'Try again !' ;;
+    esac
+
+
+
+
