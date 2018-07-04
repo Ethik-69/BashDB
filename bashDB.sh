bd132e791d47fbec2d9dc35e212dab475da16c34
Ethan Chamik <ethancmk@hotmail.fr>
2018-07-04 17:02:45 +0200

    switchcase

--- bashDB.sh
+++ bashDB.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 
-case $1 in
-    -w)
-        echo 'Write' ;;
-    -r)
-        echo 'read' ;;
-    -d)
-        echo 'delete' ;;
-    *)
-        echo 'Try again !' ;;
+write_to_file () {
+    echo $key $arg
+}
+
+read_file () {
+    echo $key $arg
+}
+
+delete_from_file () {
+    echo $key $arg
+}
+
+while [[ $# -gt 0 ]]
+do
+
+    key="$1"
+    arg="$2"
+
+    case $key in
+        -w)
+            write_to_file $key $arg
+            shift
+            shift
+            ;;
+        -r)
+            read_file $key $arg
+            shift
+            shift
+            ;;
+        -d)
+            delete_from_file $key $arg
+            shift
+            shift
+            ;;
+        *.db)
+            echo 'fucking db files !'
+            shift
+            ;;
+         *)
+            echo 'Try again !'
+            shift
+            ;;
     esac
-
-
-
-
+done
