d545804717b50541ff018da7e5642a3658fa3b06
Ethan Chamik <ethancmk@hotmail.fr>
2018-07-05 08:43:06 +0200

    add last arg use + check file exist

--- bashDB.sh
+++ bashDB.sh
@@ -1,36 +1,53 @@
 #!/bin/bash
 
+
 write_to_file () {
-    echo $key $arg
+    check_file_exist $file_name
+    echo $arg $file_name
 }
+
 
 read_file () {
-    echo $key $arg
+    echo $arg $file_name
 }
 
+
 delete_from_file () {
-    echo $key $arg
+    echo $arg $file_name
 }
+
+
+check_file_exist () {
+    if [ ! -e $file_name ]
+    then
+        echo $file_name' do not exist ! Oh My F****** God !'
+        touch $file_name
+        echo 'File Touched !'
+    fi
+}
+
 
 while [[ $# -gt 0 ]]
 do
+    ar=$#
+    file_name=${!ar}
 
     key="$1"
     arg="$2"
 
     case $key in
         -w)
-            write_to_file $key $arg
+            write_to_file $arg $file_name
             shift
             shift
             ;;
         -r)
-            read_file $key $arg
+            read_file $arg $file_name
             shift
             shift
             ;;
         -d)
-            delete_from_file $key $arg
+            delete_from_file $arg $file_name
             shift
             shift
             ;;
